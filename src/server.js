40657a5c3e76ccba1d4324df8300548563f01caa
Pedro Queiroz <pedrov-queiroz@hotmail.com>
2020-10-18 18:41:28 -0300

    quarto dia de workshop

---
+++ src/server.js
@@ -0,0 +1,24 @@
+//importar dependencia 
+const express = require('express') ;
+const path = require('path')
+const pages = require('./pages.js')
+//iniciar o express
+const server = express();
+server.use(express.static('public')) 
+
+//config template engine 
+
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'hbs')
+
+//criar rotas  
+
+.get("/", pages.index)
+.get("/orphanage", pages.orphanage)
+.get("/orphanages", pages.orphanages)
+.get("/create-orphanage",pages.createOrphanage)
+
+
+
+// ligar o servidor 
+server.listen(5500)
